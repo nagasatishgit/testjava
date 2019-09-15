efeb91ac4837f561a44b302cc3bdab8152a2901c
sagar.n <sagar.n@FGLPT407.phoenix.in>
2019-09-15 20:00:34 +0530

    commited new

--- src/main/java/com/jcg/maven/App.java
+++ src/main/java/com/jcg/maven/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        ystem.out.println( "Hello World! hai" );
+        System.out.println( "Hello World! jyhfmhhai" );
     }
 }
